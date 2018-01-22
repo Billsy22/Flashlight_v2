2c445b5e3f34a98450e63c2a0a2120fe06402b68
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-01-22 14:57:03 -0700

    Created FlashlightVC, Organized Navigator window

---
+++ Flashlight_v2/View Controllers/FlashlightViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FlashlightViewController.swift
+//  Flashlight_v2
+//
+//  Created by Taylor Bills on 1/22/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+import UIKit
+
+class FlashlightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
