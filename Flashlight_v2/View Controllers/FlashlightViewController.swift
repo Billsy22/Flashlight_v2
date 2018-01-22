179a90cbd90935468c494f0446775f0262bf7387
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-01-22 15:29:52 -0700

    Completed FlashlightViewController

--- Flashlight_v2/View Controllers/FlashlightViewController.swift
+++ Flashlight_v2/View Controllers/FlashlightViewController.swift
@@ -10,7 +10,38 @@ import UIKit
 
 class FlashlightViewController: UIViewController {
 
+    // MARK:    Properties
+    
+    var isOn = true
+    
+    @IBOutlet weak var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK:    Main Functions
+    
+    // Toggle
+    func toggleIsOn() -> Bool {
+        if isOn == false {
+            view.backgroundColor = UIColor.white
+            toggleButton.setTitle("OFF", for: .normal)
+            toggleButton.setTitleColor(.black, for: .normal)
+        } else {
+            view.backgroundColor = .black
+            toggleButton.setTitle("ON", for: .normal)
+            toggleButton.setTitleColor(.white, for: .normal)
+        }
+        return isOn
+    }
+    
+    // MARK:    Actions
+    
+    // On button tapped
+    @IBAction func buttonTapped(_ sender: Any) {
+        isOn = !isOn
+        toggleIsOn()
+    }
 }
+
